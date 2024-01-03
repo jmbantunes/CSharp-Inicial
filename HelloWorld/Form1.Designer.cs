8005b4e21a0fbf702e6dd98bb892a8cc1224e66f
João Antunes <jmbantunes2@gmail.com>
2024-01-03 18:51:14 +0000

    Adicionar arquivos de projeto.

---
+++ HelloWorld/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_clique = new System.Windows.Forms.Button();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_apelido = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_idade = new System.Windows.Forms.TextBox();
+            this.btn_limpar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_clique
+            // 
+            this.btn_clique.Location = new System.Drawing.Point(8, 173);
+            this.btn_clique.Name = "btn_clique";
+            this.btn_clique.Size = new System.Drawing.Size(183, 37);
+            this.btn_clique.TabIndex = 0;
+            this.btn_clique.Text = "Clique Aqui!!";
+            this.btn_clique.UseVisualStyleBackColor = true;
+            this.btn_clique.Click += new System.EventHandler(this.btn_clique_Click);
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(65, 29);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(93, 22);
+            this.txt_nome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Apelido";
+            // 
+            // txt_apelido
+            // 
+            this.txt_apelido.Location = new System.Drawing.Point(65, 57);
+            this.txt_apelido.Name = "txt_apelido";
+            this.txt_apelido.Size = new System.Drawing.Size(93, 22);
+            this.txt_apelido.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Idade";
+            // 
+            // txt_idade
+            // 
+            this.txt_idade.Location = new System.Drawing.Point(65, 85);
+            this.txt_idade.Name = "txt_idade";
+            this.txt_idade.Size = new System.Drawing.Size(93, 22);
+            this.txt_idade.TabIndex = 5;
+            // 
+            // btn_limpar
+            // 
+            this.btn_limpar.Location = new System.Drawing.Point(8, 130);
+            this.btn_limpar.Name = "btn_limpar";
+            this.btn_limpar.Size = new System.Drawing.Size(183, 37);
+            this.btn_limpar.TabIndex = 7;
+            this.btn_limpar.Text = "Limpar";
+            this.btn_limpar.UseVisualStyleBackColor = true;
+            this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn_clique);
+            this.groupBox1.Controls.Add(this.btn_limpar);
+            this.groupBox1.Controls.Add(this.txt_nome);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_apelido);
+            this.groupBox1.Controls.Add(this.txt_idade);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 221);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informações Pessoais";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(218, 238);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_clique;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_apelido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_idade;
+        private System.Windows.Forms.Button btn_limpar;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ HelloWorld/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Código que queremos que ele execute quando abre o programa
+        }
+
+        private void btn_clique_Click(object sender, EventArgs e)
+        {
+            //Código que queremos executar ao clicar no botão
+
+            string nome = txt_nome.Text;
+            
+            //Mandar mensagem para o ecrâ
+            MessageBox.Show("Bem vindo " + nome, "Mensagem de Boas Vindas", MessageBoxButtons.OK , MessageBoxIcon.Information);
+            //Estrutura da MessageBox -> Texto, título, botões, ícons
+         
+        }
+
+        private void btn_limpar_Click(object sender, EventArgs e)
+        {
+            //Código que acontece ao clicar no botão Limpar
+
+            //Limpar Caixas de texto através do método Clear()
+            txt_nome.Clear();
+            txt_apelido.Clear();
+            txt_idade.Clear();
+
+            //Limpar Caixas de texto através da propriedade Text
+            txt_nome.Text = "";
+
+            //Atribuir valores a caixas de texto através da propriedade Text
+            txt_nome.Text = "Alberto";
+             
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            //Código executado ao clicar na LinkLabel
+            System.Diagnostics.Process.Start("www.youtube.com");
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
